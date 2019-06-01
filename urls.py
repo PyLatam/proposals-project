62a5a0ec7833497d435cc55e480274d72cbdb5cf
Paulo <commonzenpython@gmail.com>
2019-06-02 00:52:22 +0200

    No more i18n on urls

--- urls.py
+++ urls.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from django.urls import include
 import social_django.urls
 import accounts.urls
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('proposals.urls')),
     path('accounts/', include(accounts.urls)),
     path('accounts/', include(social_django.urls)),
-    prefix_default_language=False,
-)
+]
